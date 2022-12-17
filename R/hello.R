e2c1a577cde229c1c86675d1443cb8f5756c2c0b
Caffery Yang <cafferychen7850@gmail.com>
2022-12-17 18:58:46 +0800

    使用renv进行环境初始化

---
+++ R/hello.R
@@ -0,0 +1,22 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
+library(devtools)
+use_git()
+#> ✔ Initialising Git repo
+#> ✔ Adding '.Rhistory', '.Rdata', '.httr-oauth', '.DS_Store' to '.gitignore'
