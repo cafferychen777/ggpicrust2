26c7c4bfab14e8fd193d6b7f9704256bf059a4bd
Caffery Yang <cafferychen7850@gmail.com>
2022-12-19 18:02:37 +0800

    almost finish ko2kegg_abundance and pathway_annotation

---
+++ R/ko2kegg_abundance.R
@@ -0,0 +1,27 @@
+#file="/Users/apple/Microbiome/C9orf72/Code And Data/picrust2_out/KO_metagenome_out/pred_metagenome_unstrat.tsv/pred_metagenome_unstrat.tsv"
+ko2kegg_abundance <- function(df, file){
+  message("Calculation may take a long time, please be patient.")
+  library(readr)
+  library(dplyr)
+  library(tidyr)
+  library(tibble)
+  abundance <- read_delim(file, delim = "\t", escape_double = FALSE, trim_ws = TRUE)
+  load("/Users/apple/Microbiome/ggpicrust2/ggpicrust2/data/kegg_reference.RData")
+  sample_names <- colnames(abundance)[-1]
+  kegg_names <- ko_to_kegg_reference[,1]
+  kegg_abundance <- matrix(NA,nrow = nrow(kegg_names),ncol= length(sample_names))
+  colnames(kegg_abundance) <- sample_names
+  rownames(kegg_abundance) <- as.matrix(kegg_names)
+  for (i in 1:nrow(kegg_abundance)){
+    for (j in 1:ncol(kegg_abundance)){
+      kegg_name <- rownames(kegg_abundance)[i]
+      sample_name <- colnames(kegg_abundance)[j]
+      ko_to_kegg <- ko_to_kegg_reference[ko_to_kegg_reference[,1]==kegg_name,-1]
+      ko_to_kegg <- ko_to_kegg[!is.na(ko_to_kegg)]
+      kegg_abundance[i,j] <- sum(abundance[as.matrix(abundance[,1]) %in% ko_to_kegg,sample_name])
+    }
+  }
+  kegg_abundance <- kegg_abundance[rowSums(kegg_abundance) != 0, ]
+  message("The kegg pathway with zero abundance in all the different samples has been removed.")
+  return(kegg_abundance)
+}
--- R/pathway_annotation.R
+++ R/pathway_annotation.R
@@ -1,6 +1,8 @@
 #file=.tsv or .txt. or .csv
+#pathway="KO", "EC" and "MetaCyc"
 #file="/Users/apple/Microbiome/C9orf72/Code And Data/picrust2_out/EC_metagenome_out/pred_metagenome_unstrat.tsv/pred_metagenome_unstrat.tsv"
 #file="/Users/apple/Microbiome/C9orf72/Code And Data/picrust2_out/KO_metagenome_out/pred_metagenome_unstrat.tsv/pred_metagenome_unstrat.tsv"
+#file="/Users/apple/Microbiome/C9orf72/Code And Data/picrust2_out/pathways_out/path_abun_unstrat.tsv/path_abun_unstrat.tsv"
 pathway_annotation <- function(file,pathway){
   library(dplyr)
   library(tibble)
@@ -10,24 +12,33 @@ pathway_annotation <- function(file,pathway){
          ".txt" = abundance <- read_delim(file, delim = "\t", escape_double = FALSE, trim_ws = TRUE),
          ".tsv" = abundance <- read_delim(file, delim = "\t", escape_double = FALSE, trim_ws = TRUE),
          ".csv" = abundance <- read_delim(file, delim = "\t", escape_double = FALSE, trim_ws = TRUE),
-          default = stop("Error: Please input file as .tsv, .txt or .csv\nThe best input file is what you get from picrust2 output file 'pred_metagenome_unstrat.tsv'"))
+          stop("Error: Please input file as .tsv, .txt or .csv\nThe best input file is what you get from picrust2 output file 'pred_metagenome_unstrat.tsv'"))
   abundance <- abundance %>% add_column(description = rep(NA,length = nrow(abundance)),.after = 1)
   switch(pathway,
-         "KO" = {load("/Users/apple/Microbiome/ggpicrust2/ggpicrust2/data/KO_reference.RData")
-           for (i in 1:nrow(abundance)){
-             abundance[i,2] <- KO_reference[KO_reference[,1] %in% abundance[i,1],5][1]
-           }}
-           ,
-         "EC" = {load("/Users/apple/Microbiome/ggpicrust2/ggpicrust2/data/EC_reference.RData")
-           for (i in 1:nrow(abundance)){
-             abundance[i,2] <- EC_reference[EC_reference[,1] %in% abundance[i,1],2]
+         "KO" = {
+           load("/Users/apple/Microbiome/ggpicrust2/ggpicrust2/data/KO_reference.RData")
+           for (i in 1:nrow(abundance)) {
+             abundance[i, 2] <-
+               KO_reference[KO_reference[, 1] %in% abundance[i, 1], 5][1]
+           }
+         }
+         ,
+         "EC" = {
+           load("/Users/apple/Microbiome/ggpicrust2/ggpicrust2/data/EC_reference.RData")
+           for (i in 1:nrow(abundance)) {
+             abundance[i, 2] <-
+               EC_reference[EC_reference[, 1] %in% abundance[i, 1], 2]
            }
            message("EC description may appear to be duplicated")
-           },
-         "MetaCyc" = {load("/Users/apple/Microbiome/ggpicrust2/ggpicrust2/data/MetaCyc_reference.RData")
-           for (i in 1:nrow(abundance)){
-             abundance[i,2] <- MetaCyc_reference[MetaCyc_reference[,1] %in% abundance[i,1],2]
-           }})
+         },
+         "MetaCyc" = {
+           load("/Users/apple/Microbiome/ggpicrust2/ggpicrust2/data/MetaCyc_reference.RData")
+           for (i in 1:nrow(abundance)) {
+             abundance[i, 2] <-
+               MetaCyc_reference[MetaCyc_reference[, 1] %in% abundance[i, 1], 2]
+           }
+         },
+         stop("Only provide 'KO', 'EC' and 'MetaCyc' pathway"))
   return(abundance)
 }
 
