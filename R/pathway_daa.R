cf80f51590c34be76a7fae92f9a1c3218f35b592
Caffery Yang <cafferychen7850@gmail.com>
2022-12-23 01:47:49 +0800

    Big progress

--- R/pathway_daa.R
+++ R/pathway_daa.R
@@ -39,22 +39,23 @@ pathway_daa <-
            "FALSE" = {
              abundance <- abundance[, colnames(abundance) %in% select]
              metadata <-
-               metadata[as.matrix(metadata[, matching_columns]) %in% select,]
+               metadata[as.matrix(metadata[, matching_columns]) %in% select, ]
            })
     sample_names <- colnames(abundance)
     abundance_mat <- as.matrix(abundance)
     metadata_order <-
       match(sample_names, as.matrix(metadata[, matching_columns]))
-    metadata <- metadata[metadata_order,]
+    metadata <- metadata[metadata_order, ]
     metadata_mat <- as.matrix(metadata)
     metadata_df <- as.data.frame(metadata)
     Group <- factor(metadata_mat[, group])
     Level <- names(table(Group))
+    length_Level <- length(Level)
     switch(
       daa_method,
       "ALDEx2" = {
         ALDEx2_abundance <- round(abundance)
-        switch(length(Level) == 2,
+        switch(length_Level == 2,
                "TRUE" = {
                  ALDEx2_object <-
                    aldex.clr(
@@ -72,8 +73,8 @@ pathway_daa <-
                    data.frame(
                      feature = rep(rownames(ALDEx2_results), 2),
                      method = c(
-                       rep("Welch’s t test", nrow(ALDEx2_results)),
-                       rep("Wilcoxon rank test", nrow(ALDEx2_results))
+                       rep("ALDEx2_Welch’s t test", nrow(ALDEx2_results)),
+                       rep("ALDEx2_Wilcoxon rank test", nrow(ALDEx2_results))
                      ),
                      group1 = rep(names(table(Group))[1], 2 * nrow(ALDEx2_results)),
                      group2 = rep(names(table(Group))[2], 2 * nrow(ALDEx2_results)),
@@ -95,19 +96,19 @@ pathway_daa <-
                    data.frame(
                      feature = rep(rownames(ALDEx2_results), 2),
                      method = c(
-                       rep("Kruskal-Wallace test", nrow(ALDEx2_results)),
-                       rep("glm test", nrow(ALDEx2_results))
+                       rep("ALDEx2_Kruskal-Wallace test", nrow(ALDEx2_results)),
+                       rep("ALDEx2_glm test", nrow(ALDEx2_results))
                      ),
                      p_values = c(ALDEx2_results$kw.ep, ALDEx2_results$glm.ep)
                    )
-                 for (k in 1:length(Level)) {
+                 for (k in 1:length_Level) {
                    p_values_df <-
                      cbind(p_values_df[, 1:(1 + k)],
                            rep(names(table(Group))[k], 2 * nrow(ALDEx2_results)),
                            p_values_df$p_values)
                  }
-                 colnames(p_values_df)[3:(3 + length(Level))] <-
-                   c(paste0("group", 1:length(Level)), "p_values")
+                 colnames(p_values_df)[3:(3 + length_Level)] <-
+                   c(paste0("group", 1:length_Level), "p_values")
                })
       },
       "DESeq2" = {
@@ -127,7 +128,7 @@ pathway_daa <-
           j <- DESeq2_combinations[, i]
           DESeq2_sub_group <-
             DESeq2_metadata$Group_group_nonsense %in% j
-          DESeq2_metadata_sub <- DESeq2_metadata[DESeq2_sub_group, ]
+          DESeq2_metadata_sub <- DESeq2_metadata[DESeq2_sub_group,]
           DESeq2_abundance_mat_sub <-
             DESeq2_abundance_mat[, DESeq2_sub_group]
           DESeq2_object <-
@@ -151,17 +152,18 @@ pathway_daa <-
         for (i in 1:length(DESeq2_results_nrow)) {
           DESeq2_group_matrix <-
             rbind(matrix(
-              rep(DESeq2_combinations_matrix_t[i, ], times = DESeq2_results_nrow[i]),
+              rep(DESeq2_combinations_matrix_t[i,], times = DESeq2_results_nrow[i]),
               ncol = 2,
               byrow = TRUE
             ), DESeq2_group_matrix)
         }
         DESeq2_group_matrix <-
-          DESeq2_group_matrix[complete.cases(DESeq2_group_matrix), ]
+          DESeq2_group_matrix[complete.cases(DESeq2_group_matrix),]
         colnames(DESeq2_group_matrix) <- c("group1", "group2")
         p_values_matrix <-
           cbind(
             feature = rownames(DESeq2_results_matrix),
+            method = "DESeq2",
             DESeq2_group_matrix,
             p_values = as.vector(DESeq2_results_matrix[, "pvalue"])
           )
@@ -174,8 +176,8 @@ pathway_daa <-
         rownames(Maaslin2_metadata_df) <-
           Maaslin2_metadata_df[, matching_columns]
         Maaslin2_metadata_df <-
-          select(Maaslin2_metadata_df,-matching_columns)
-        switch(length(Level) == 2,
+          select(Maaslin2_metadata_df, -matching_columns)
+        switch(length_Level == 2,
                "TRUE" = {
                  Maaslin2_results <- Maaslin2(
                    Maaslin2_abundance_mat,
@@ -191,6 +193,7 @@ pathway_daa <-
                  p_values_matrix <-
                    cbind(
                      feature = Maaslin2_results$results$feature,
+                     method = "Maaslin2",
                      group1 = names(table(Group))[1],
                      group2 = names(table(Group))[2],
                      p_values = Maaslin2$results$pval
@@ -212,6 +215,7 @@ pathway_daa <-
                  p_values_matrix <-
                    cbind(
                      feature = Maaslin2_results$results$feature,
+                     method = "Maaslin2",
                      group1 = Maaslin2_results$results$value,
                      group2 = reference,
                      p_values = Maaslin2$results$pval
@@ -236,7 +240,7 @@ pathway_daa <-
         rownames(LinDA_metadata_df) <-
           LinDA_metadata_df[, matching_columns]
         LinDA_metadata_df <-
-          select(LinDA_metadata_df,-matching_columns)
+          select(LinDA_metadata_df, -matching_columns)
         LinDA_metadata_df$Group_group_nonsense_ <-
           factor(LinDA_metadata_df$Group_group_nonsense_)
         LinDA_metadata_df$Group_group_nonsense_ <-
@@ -244,148 +248,192 @@ pathway_daa <-
         LinDA_results <- MicrobiomeStat::linda(abundance,
                                                LinDA_metadata_df,
                                                formula = "~Group_group_nonsense_",
-                                               alpha = 0.05,)$output
-        for (i in 1:length(LinDA_results)){
-          LinDA_results[[i]] <- cbind(feature = rownames(LinDA_results[[i]]),method= "LinDA",group1 = substr(names(LinDA_results)[i],22,stop=nchar(names(LinDA_results)[i])),group2 = reference,p_values = LinDA_results[[i]]$pvalue)
+                                               alpha = 0.05,
+        )$output
+        for (i in 1:length(LinDA_results)) {
+          LinDA_results[[i]] <-
+            cbind(
+              feature = rownames(LinDA_results[[i]]),
+              method = "LinDA",
+              group1 = substr(names(LinDA_results)[i], 22, stop = nchar(names(
+                LinDA_results
+              )[i])),
+              group2 = reference,
+              p_values = LinDA_results[[i]]$pvalue
+            )
         }
-
         p_values_matrix <- as.matrix(do.call(rbind, LinDA_results))
         p_values_df <- as.data.frame(p_values_matrix)
       },
       "edgeR" = {
-        edgeR_abundance_mat <- t(round(abundance_mat))
-        edgeR_object <-
-          DGEList(counts = edgeR_abundance_mat, group = Group)
-        edgeR_object <- calcNormFactors(edgeR_object)
-        edgeR_object <-
-          estimateCommonDisp(edgeR_object, verbose = T)
-        length_Level <- length(Level)
-        switch(length_Level == 2,
-               "TRUE" = {
-                 edgeR_results <- exactTest(edgeR_object, pair = c(1, 2))
-                 p_values <-
-                   as.matrix(edgeR_results$table)[,-c(1, 2)]
-               },
-               {
-                 edgeR_results <- list()
-                 numbers <- seq_along(Level)
-                 combinations <- combn(numbers, 2)
-                 for (j in 1:sum(1:(length(Level) - 1))) {
-                   DGEExact <- exactTest(edgeR_object, pair = combinations[, j])
-                   edgeR_results[[j]] <- tibble(
-                     feature = rownames(DGEExact$table),
-                     Group1 = DGEExact$comparison[1],
-                     Group2 = DGEExact$comparison[2],
-                     PValue = DGEExact$table$PValue
-                   )
-                 }
-                 edgeR_results <- bind_rows(edgeR_results)
-               })
-      },
-      "limma voom" = {
         edgeR_abundance_mat <- round(abundance_mat)
         edgeR_object <-
           DGEList(counts = edgeR_abundance_mat, group = Group)
-        edgeR_object <- calcNormFactors(edgeR_object)
-        limma_voom_Fit <- lmFit(voom(edgeR_object))
+        edgeR_object <- edgeR::calcNormFactors(edgeR_object)
+        edgeR_object <-
+          estimateCommonDisp(edgeR_object, verbose = T)
+        switch(length_Level == 2,
+               "TRUE" = {
+                 edgeR_results <- exactTest(edgeR_object, pair = c(1, 2))
+                 p_values_matrix <-
+                   cbind(
+                     feature = rownames(edgeR_abundance_mat),
+                     method = "edgeR",
+                     group1 = Level[1],
+                     group2 = Level[2],
+                     p_values = edgeR_results$table$PValue
+                   )
+                 p_values_df <- as.data.frame(p_values_matrix)
+               },
+               {
+                 edgeR_results <- list()
+                 edgeR_numbers <- seq_along(Level)
+                 edgeR_combinations <- combn(edgeR_numbers, 2)
+                 for (j in 1:sum(1:(length_Level - 1))) {
+                   DGEExact <- exactTest(edgeR_object, pair = edgeR_combinations[, j])
+                   edgeR_results[[j]] <- cbind(
+                     feature = rownames(DGEExact$table),
+                     method = "edgeR",
+                     group1 = DGEExact$comparison[1],
+                     group2 = DGEExact$comparison[2],
+                     p_values = DGEExact$table$PValue
+                   )
+                 }
+                 p_values_matrix <-
+                   as.matrix(do.call(rbind, edgeR_results))
+                 p_values_df <- as.data.frame(p_values_matrix)
+               })
+      },
+      "limma voom" = {
+        limma_voom_abundance_mat <- round(abundance_mat)
+        Group <- relevel(Group, ref = reference)
+        limma_voom_object <-
+          DGEList(counts = edgeR_abundance_mat, group = Group)
+        limma_voom_object <-
+          edgeR::calcNormFactors(limma_voom_object)
+        limma_voom_Fit <- lmFit(voom(limma_voom_object))
         limma_voom_Fit <- eBayes(limma_voom_Fit)
-        p_values <- limma_voom_Fit$p.value[, 2]
+        p_values_matrix <-
+          cbind(
+            feature = rep(rownames(abundance), length_Level - 1),
+            method = "limma voom",
+            group1 = reference,
+            group2 = rep(substr(
+              colnames(limma_voom_Fit$p.value)[-1], 6, nchar(colnames(limma_voom_Fit$p.value)[-1])
+            ), each = nrow(abundance)),
+            p_values = c(limma_voom_Fit$p.value[, -1])
+          )
+        p_values_df <- as.data.frame(p_values_matrix)
       },
       "metagenomeSeq" = {
-        if (length(Level) != 2) {
-          stop("MetagenomeSeq only support two groups comparison.")
+        metagenomeSeq_combinations <- combn(Level, 2)
+        metagenomeSeq_results_list <- list()
+        for (i in seq_len(ncol(metagenomeSeq_combinations))) {
+          sub_metadata_df <-
+            metadata_df[metadata_df[, group] %in% metagenomeSeq_combinations[, i],]
+          sub_abundance <-
+            abundance[, metadata_df[, group] %in% metagenomeSeq_combinations[, i]]
+          metagenomeSeq_list <- list()
+          metagenomeSeq_list[[1]] <- sub_abundance
+          metagenomeSeq_list[[2]] <-
+            as.data.frame(rownames(sub_abundance))
+          metagenomeSeq_metadata_df <- sub_metadata_df
+          rownames(metagenomeSeq_metadata_df) <-
+            metagenomeSeq_metadata_df[, matching_columns]
+          metagenomeSeq_metadata_df <-
+            select(metagenomeSeq_metadata_df, -matching_columns)
+          metagenomeSeq_colnames <-
+            colnames(metagenomeSeq_metadata_df)
+          metagenomeSeq_colnames[metagenomeSeq_colnames == group] <-
+            "Group_group_nonsense_"
+          colnames(metagenomeSeq_metadata_df) <-
+            metagenomeSeq_colnames
+          metagenomeSeq_phenotypeData <-
+            AnnotatedDataFrame(metagenomeSeq_metadata_df)
+          metagenomeSeq_object <-
+            newMRexperiment(metagenomeSeq_list[[1]], phenoData = metagenomeSeq_phenotypeData)
+          metagenomeSeq_object <-
+            cumNorm(metagenomeSeq_object, p = cumNormStatFast(metagenomeSeq_object))
+          metagenomeSeq_mod <-
+            model.matrix(~ Group_group_nonsense_, data = pData(metagenomeSeq_object))
+          metagenomeSeq_results <-
+            fitFeatureModel(metagenomeSeq_object, metagenomeSeq_mod)
+          metagenomeSeq_results_list[[i]] <-
+            cbind(
+              feature = names(metagenomeSeq_results@pvalues),
+              method = "metagenomeSeq",
+              group1 = metagenomeSeq_combinations[, i][1],
+              group2 = metagenomeSeq_combinations[, i][2],
+              p_values = as.vector(metagenomeSeq_results@pvalues)
+            )
         }
-        metagenomeSeq_list <- list()
-        metagenomeSeq_list[[1]] <- abundance
-        metagenomeSeq_list[[2]] <-
-          as.data.frame(rownames(abundance))
-        metagenomeSeq_metadata_df <- metadata_df
-        rownames(metagenomeSeq_metadata_df) <-
-          metagenomeSeq_metadata_df[, matching_columns]
-        metagenomeSeq_metadata_df <-
-          select(metagenomeSeq_metadata_df,-matching_columns)
-        metagenomeSeq_colnames <-
-          colnames(metagenomeSeq_metadata_df)
-        metagenomeSeq_colnames[metagenomeSeq_colnames == group] <-
-          "Group_group_nonsense_"
-        colnames(metagenomeSeq_metadata_df) <-
-          metagenomeSeq_colnames
-        metagenomeSeq_phenotypeData <-
-          AnnotatedDataFrame(metagenomeSeq_metadata_df)
-        metagenomeSeq_taxa <- data.frame(OTU = rownames(abundance))
-        rownames(metagenomeSeq_taxa) <- rownames(abundance)
-        metagenomeSeq_OTUdata <-
-          AnnotatedDataFrame(metagenomeSeq_taxa)
-        metagenomeSeq_object <-
-          newMRexperiment(metagenomeSeq_list[[1]], phenoData = metagenomeSeq_phenotypeData)
-        metagenomeSeq_object <-
-          cumNorm(metagenomeSeq_object, p = cumNormStatFast(metagenomeSeq_object))
-        metagenomeSeq_mod <-
-          model.matrix( ~ Group_group_nonsense_, data = pData(metagenomeSeq_object))
-        metagenomeSeq_results <-
-          fitFeatureModel(metagenomeSeq_object, metagenomeSeq_mod)
-        p_values <- metagenomeSeq_results@pvalues
-      },
+        p_values_matrix <-
+          as.matrix(do.call(rbind, p_values_list))
+        p_values_df <- as.data.frame(p_values_matrix)
+      }
+      ,
       "Lefser" = {
-        # Lefser only for two groups.
-        if (length(Level) != 2) {
-          stop("Lefser only support two groups comparison.")
+        Lefser_combinations <- combn(Level, 2)
+        Lefser_results <- list()
+        Lefser_metadata_df <- metadata_df
+        for (i in seq_len(ncol(Lefser_combinations))) {
+          Lefser_sub_metadata_df <-
+            Lefser_metadata_df[Lefser_metadata_df[, group] %in% Lefser_combinations[, i],]
+          Lefser_sub_abundance <-
+            abundance[, Lefser_metadata_df[, group] %in% Lefser_combinations[, i]]
+          colnames(Lefser_sub_metadata_df)[colnames(Lefser_sub_metadata_df) == group] <-
+            "Group_group_nonsense_"
+          Lefser_sub_metadata_df$Group_group_nonsense_ <-
+            factor(Lefser_sub_metadata_df$Group_group_nonsense_)
+          Lefser_object <-
+            SummarizedExperiment(assays = list(counts = as.matrix(Lefser_sub_abundance)),
+                                 colData = Lefser_sub_metadata_df)
+          Lefser_kw_filter <-
+            apply(Lefser_object@assays@data$counts, 1L, function(x) {
+              kruskal.test(x ~ as.numeric(Lefser_sub_metadata_df[, "Group_group_nonsense_"]) -
+                             1)[["p.value"]]
+            })
+          if (!sum(na.omit(as.numeric(Lefser_kw_filter < 0.05)))) {
+            next
+          }
+          Lefser_results[[i]] <-
+            cbind(
+              feature = lefser(Lefser_object, groupCol = "Group_group_nonsense_")$Names,
+              method = "Lefser",
+              group1 = Lefser_combinations[, 1][1],
+              group2 = Lefser_combinations[, 1][2],
+              effect_scores = lefser(Lefser_object, groupCol = "Group_group_nonsense_")$scores
+            )
         }
-        BiocManager::install("lefser")
-        Lefser_object <-
-          SummarizedExperiment(assays = list(counts = abundance), colData = metadata_df)
-        Lefser_results <- lefser(se1, groupCol = "Enviroment")
-        lefserPlot(
-          Lefser_results,
-          colors = c("#7fb1d3", "#fdb462"),
-          trim.names = FALSE
-        )
+        p_values_matrix <-
+          as.matrix(do.call(rbind, Lefser_results))
       }
     )
-
-
-
-
     switch(
       p.adjust,
       "BH" = {
-        adjusted_p_values <- p.adjust(p_values, method = "BH")
+        adjusted_p_values <- p.adjust(p_values_df$p_values, method = "BH")
       },
       "holm" = {
-        adjusted_p_values <- p.adjust(p_values, method = "holm")
+        adjusted_p_values <- p.adjust(p_values_df$p_values, method = "holm")
       },
       "bonferroni" = {
-        adjusted_p_values <- p.adjust(p_values, method = "bonferroni")
+        adjusted_p_values <-
+          p.adjust(p_values_df$p_values, method = "bonferroni")
       },
-      "Bayes" = {
-        library(rstan)
-        model_string <- "
-  data {
-    int<lower=0> N;
-    real p[N];
-  }
-  parameters {
-    real<lower=0,upper=1> mu;
-  }
-  model {
-    for (i in 1:N) {
-      p[i] ~ beta(mu, 1);
-    }
-  }
-"
-        model <- stan_model(model_code = model_string)
-        posterior <-
-          sampling(model, data = list(N = length(p_values), p = p_values))
+      "hochberg" = {
+        adjusted_p_values <-
+          p.adjust(p_values_df$p_values, method = "hochberg")
       },
-      "Random Effect" = {
-        # 加载必要的包
-        library(glmnet)
-        # 将 p 值转换为二进制类别
-        y <- ifelse(p_values < 0.05, 1, 0)
-        # 建立模型
-        model <- cv.glmnet(x, y, family = "binomial")
-        # 计算系数
-        coefs <- coef(model, s = "lambda.min")
+      "fdr" = {
+        adjusted_p_values <-
+          p.adjust(p_values_df$p_values, method = "fdr")
+      },
+      "none" = {
+        adjusted_p_values <-
+          p.adjust(p_values_df$p_values, method = "none")
       }
     )
+    daa_results_df <- cbind(p_values_df,adj_method="BH",p_adjust=adjusted_p_values)
+    return(daa_results_df)
   }
--- R/pathway_errorbar.R
+++ R/pathway_errorbar.R
@@ -1,3 +1,7 @@
 pathway_errorbar <- function(){
-
+  lefserPlot(
+    Lefser_results,
+    colors = c("#7fb1d3", "#fdb462"),
+    trim.names = FALSE
+  )
 }
