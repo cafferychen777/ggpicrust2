4ed6ba0a3f5123b757a441a6c2d8ad81ae83d48d
Caffery Yang <cafferychen7850@gmail.com>
2022-12-27 01:09:10 +0800

    big change

--- R/pathway_annotation.R
+++ R/pathway_annotation.R
@@ -2,68 +2,99 @@
 #'
 #' @param file
 #' @param pathway
+#' @param daa_results_df
 #'
 #' @return
 #' @export
 #'
 #' @examples
-pathway_annotation <- function(file, pathway) {
-  file_format <- substr(file, nchar(file) - 3, nchar(file))
-  switch(file_format,
-    ".txt" = abundance <-
-      read_delim(
-        file,
-        delim = "\t",
-        escape_double = FALSE,
-        trim_ws = TRUE
-      ),
-    ".tsv" = abundance <-
-      read_delim(
-        file,
-        delim = "\t",
-        escape_double = FALSE,
-        trim_ws = TRUE
-      ),
-    ".csv" = abundance <-
-      read_delim(
-        file,
-        delim = "\t",
-        escape_double = FALSE,
-        trim_ws = TRUE
-      ),
-    stop(
-      "Error: Please input file as .tsv, .txt or .csv\nThe best input file is what you get from picrust2 output file 'pred_metagenome_unstrat.tsv'"
-    )
-  )
-  abundance <-
-    abundance %>% add_column(
-      description = rep(NA, length = nrow(abundance)),
-      .after = 1
-    )
-  switch(pathway,
-    "KO" = {
-      load(system.file("extdata", "KO_reference.RData", package = "ggpicrust2"))
-      for (i in seq_len(nrow(abundance))) {
-        abundance[i, 2] <-
-          KO_reference[KO_reference[, 1] %in% abundance[i, 1], 5][1]
+pathway_annotation <-
+  function(file = NULL,
+           pathway = NULL,
+           daa_results_df = NULL) {
+    if (is.null(file) & is.null(daa_results_df)) {
+      stop("Please input the picrust2 output or results of pathway_daa daa_results_df")
+    }
+    if (!is.null(file)) {
+      file_format <- substr(file, nchar(file) - 3, nchar(file))
+      switch(
+        file_format,
+        ".txt" = abundance <-
+          read_delim(
+            file,
+            delim = "\t",
+            escape_double = FALSE,
+            trim_ws = TRUE
+          ),
+        ".tsv" = abundance <-
+          read_delim(
+            file,
+            delim = "\t",
+            escape_double = FALSE,
+            trim_ws = TRUE
+          ),
+        ".csv" = abundance <-
+          read_delim(
+            file,
+            delim = "\t",
+            escape_double = FALSE,
+            trim_ws = TRUE
+          ),
+        stop(
+          "Error: Please input file as .tsv, .txt or .csv\nThe best input file is what you get from picrust2 output file 'pred_metagenome_unstrat.tsv'"
+        )
+      )
+      abundance <-
+        abundance %>% add_column(description = rep(NA, length = nrow(abundance)),
+                                 .after = 1)
+      switch(
+        pathway,
+        "KO" = {
+          load(system.file("extdata", "KO_reference.RData", package = "ggpicrust2"))
+          for (i in seq_len(nrow(abundance))) {
+            abundance[i, 2] <-
+              KO_reference[KO_reference[, 1] %in% abundance[i, 1], 5][1]
+          }
+        },
+        "EC" = {
+          load(system.file("extdata", "EC_reference.RData", package = "ggpicrust2"))
+          for (i in seq_len(nrow(abundance))) {
+            abundance[i, 2] <-
+              EC_reference[EC_reference[, 1] %in% abundance[i, 1], 2]
+          }
+          message("EC description may appear to be duplicated")
+        },
+        "MetaCyc" = {
+          load(system.file("extdata", "MetaCyc_reference.RData", package = "ggpicrust2"))
+          for (i in seq_len(nrow(abundance))) {
+            abundance[i, 2] <-
+              MetaCyc_reference[MetaCyc_reference[, 1] %in% abundance[i, 1], 2]
+          }
+        },
+        stop("Only provide 'KO', 'EC' and 'MetaCyc' pathway")
+      )
+      return(abundance)
+    }
+    if (!is.null(daa_results_df)){
+      daa_results_filtered_df <-
+        daa_results_df[daa_results_df$p_adjust < 0.05, ]
+      daa_results_filtered_df$pathway_name <- "nonsense"
+      daa_results_filtered_df$pathway_description <- "nonsense"
+      daa_results_filtered_df$pathway_class <- "nonsense"
+      daa_results_filtered_df$pathway_map <- "nonsense"
+      keggGet_results <- list()
+      for (i in seq_len(nrow(daa_results_filtered_df))) {
+        keggGet_results[[i]] <- keggGet(daa_results_filtered_df$feature[i])
+        daa_results_filtered_df[i, ]$pathway_name <-
+          keggGet_results[[i]][[1]]$NAME
+        daa_results_filtered_df[i, ]$pathway_description <-
+          keggGet_results[[i]][[1]]$DESCRIPTION
+        daa_results_filtered_df[i, ]$pathway_class <-
+          keggGet_results[[i]][[1]]$CLASS
+        daa_results_filtered_df[i, ]$pathway_map <-
+          keggGet_results[[i]][[1]]$PATHWAY_MAP
       }
-    },
-    "EC" = {
-      load(system.file("extdata", "EC_reference.RData", package = "ggpicrust2"))
-      for (i in seq_len(nrow(abundance))) {
-        abundance[i, 2] <-
-          EC_reference[EC_reference[, 1] %in% abundance[i, 1], 2]
-      }
-      message("EC description may appear to be duplicated")
-    },
-    "MetaCyc" = {
-      load(system.file("extdata", "MetaCyc_reference.RData", package = "ggpicrust2"))
-      for (i in seq_len(nrow(abundance))) {
-        abundance[i, 2] <-
-          MetaCyc_reference[MetaCyc_reference[, 1] %in% abundance[i, 1], 2]
-      }
-    },
-    stop("Only provide 'KO', 'EC' and 'MetaCyc' pathway")
-  )
-  return(abundance)
-}
+      daa_results_filtered_annotation_df <- daa_results_filtered_df
+      return(daa_results_filtered_annotation_df)
+    }
+  }
--- R/pathway_errorbar.R
+++ R/pathway_errorbar.R
@@ -3,22 +3,34 @@ pathway_errorbar <-
            daa_results_df,
            Group,
            p_values_threshold = 0.05,
-           order = ,
+           order = "pathway_class",
            select = NULL,
-           boxplot = TRUE) {
+           p_value_bar = TRUE,
+           colors = NULL,
+           x_lab = "pathway_name") {
+    if (is.null(colors)) {
+      colors <- c("#d93c3e", "#3685bc", "#87ceeb")
+    }
     errorbar_abundance_mat <- as.matrix(abundance)
-    daa_resuls_filtered_df <-
+    daa_results_filtered_df <-
       daa_results_df[daa_results_df$p_adjust < p_values_threshold, ]
-    errorbar_relative_abundance_mat <-
-      make_relative(errorbar_abundance_mat)
-    errorbar_relative_sub_abundance_mat <-
-      errorbar_relative_abundance_mat[rownames(errorbar_relative_abundance_mat) %in% daa_resuls_filtered_df$feature, ]
-    #这里可能要加一个排序
+    if (!is.null(select)) {
+      daa_results_filtered_sub_df <-
+        daa_results_filtered_df[daa_results_filtered_df$feature %in% select,]
+    } else {
+      daa_results_filtered_sub_df <- daa_results_filtered_df
+    }
+    errorbar_sub_abundance_mat <-
+      errorbar_abundance_mat[rownames(errorbar_abundance_mat) %in% daa_results_filtered_sub_df$feature, ]
+    errorbar_sub_abundance_mat <-
+      errorbar_sub_abundance_mat[rownames(daa_results_filtered_sub_df),]
+    errorbar_sub_relative_abundance_mat <-
+      make_relative(errorbar_sub_abundance_mat)
     error_bar_matrix <-
       cbind(
-        sample = colnames(errorbar_relative_sub_abundance_mat),
+        sample = colnames(errorbar_sub_relative_abundance_mat),
         group = Group,
-        t(errorbar_relative_sub_abundance_mat)
+        t(errorbar_sub_relative_abundance_mat)
       )
     error_bar_df <- as.data.frame(error_bar_matrix)
     error_bar_pivot_longer_df <-
@@ -36,12 +48,81 @@ pathway_errorbar <-
       summarise(mean = mean(value), sd = sd(value))
     error_bar_pivot_longer_tibble_summarised <-
       error_bar_pivot_longer_tibble_summarised %>% mutate(group2 = "nonsense")
+    switch(
+      order,
+      "p_values" = {
+        order <- order(daa_results_filtered_sub_df$p_adjust)
+      },
+      "name" = {
+        order <- order(daa_results_filtered_sub_df$feature)
+      },
+      "group" = {
+        daa_results_filtered_sub_df$pro <- 1
+        for (i in levels(error_bar_pivot_longer_tibble_summarised$name)) {
+          error_bar_pivot_longer_tibble_summarised_sub <-
+            error_bar_pivot_longer_tibble_summarised[error_bar_pivot_longer_tibble_summarised$name ==
+                                                       i, ]
+          pro_group <-
+            error_bar_pivot_longer_tibble_summarised_sub[error_bar_pivot_longer_tibble_summarised_sub$mean ==
+                                                           max(error_bar_pivot_longer_tibble_summarised_sub$mean), ]$group
+          pro_group <- as.vector(pro_group)
+          daa_results_filtered_sub_df[daa_results_filtered_sub_df$feature ==
+                                        i, ]$pro <- pro_group
+        }
+        order <-
+          order(daa_results_filtered_sub_df$pro,
+                daa_results_filtered_sub_df$p_adjust)
+      },
+      "pathway_class" = {
+        if (!"pathway_class" %in% colnames(daa_results_filtered_sub_df)) {
+          stop("Please use pathway_annotation function to annotation the pathway_daa results")
+        }
+        order <- order(
+          daa_results_filtered_sub_df$pathway_class,
+          daa_results_filtered_sub_df$p_adjust
+        )
+      },
+      {
+        order <- order
+      }
+    )
+    daa_results_filtered_sub_df <-
+      daa_results_filtered_sub_df[order, ]
+    error_bar_pivot_longer_tibble_summarised_ordered <-
+      data.frame(
+        name = NULL,
+        group = NULL,
+        mean = NULL,
+        sd = NULL
+      )
+    for (i in daa_results_filtered_sub_df$feature) {
+      error_bar_pivot_longer_tibble_summarised_ordered <-
+        rbind(
+          error_bar_pivot_longer_tibble_summarised_ordered,
+          error_bar_pivot_longer_tibble_summarised[error_bar_pivot_longer_tibble_summarised$name ==
+                                                     i, ]
+        )
+    }
+    levels(error_bar_pivot_longer_tibble_summarised_ordered$name) <-
+      rev(daa_results_filtered_sub_df$feature)
+    error_bar_pivot_longer_tibble_summarised_ordered$pathway_name <-
+      rep(daa_results_filtered_sub_df$pathway_name, each = length(levels(
+        factor(error_bar_pivot_longer_tibble_summarised_ordered$group)
+      )))
+    error_bar_pivot_longer_tibble_summarised_ordered$pathway_class <-
+      rep(daa_results_filtered_sub_df$pathway_class, each = length(levels(
+        factor(error_bar_pivot_longer_tibble_summarised_ordered$group)
+      )))
+    levels(error_bar_pivot_longer_tibble_summarised_ordered$pathway_name) <-
+      rev(daa_results_filtered_sub_df$pathway_name)
+
+
 
     bar_errorbar <-
-      ggplot(error_bar_pivot_longer_tibble_summarised,
-             aes(name, mean, fill = group)) +
+      ggplot(error_bar_pivot_longer_tibble_summarised_ordered,
+             aes(mean, name, fill = group)) +
       geom_errorbar(
-        aes(ymax = mean + sd, ymin = 0),
+        aes(xmax = mean + sd, xmin = 0),
         position = position_dodge(width = 0.8),
         width = 0.5,
         size = 0.5,
@@ -51,13 +132,16 @@ pathway_errorbar <-
                position = position_dodge(width = 0.8),
                width = 0.8) +
       geom_stripped_cols() +
-      scale_fill_manual(values = c("#fdb462", "#7fb1d3")) +
-      scale_color_manual(values = c("#fdb462", "#7fb1d3")) +
+      scale_fill_manual(values = c(colors[1], colors[2])) +
+      scale_color_manual(values = c(colors[1], colors[2])) +
       theme_prism() +
-      scale_y_continuous(expand = c(0, 0),
-                         #limits = c(0, 0.1),
-                         guide = "prism_offset_minor") +
-      labs(y = "Relative Abundance(%)", x = NULL) +
+      scale_x_continuous(
+        expand = c(0, 0),
+        guide = "prism_offset_minor",
+        limits = c(0, 0.03375004)
+      ) +
+      scale_y_discrete(labels = rev(daa_results_filtered_sub_df$pathway_name)) +
+      labs(x = "Relative Abundance(%)", y = NULL) +
       theme(
         axis.ticks.y = element_blank(),
         axis.line.y = element_blank(),
@@ -66,20 +150,27 @@ pathway_errorbar <-
         panel.grid.major.y = element_blank(),
         panel.grid.major.x = element_blank(),
         axis.text = element_text(size = 10, color = "black"),
-        axis.text.y = element_text(margin = margin(r = 0)),
-        axis.text.x = element_text(
+        axis.text.x = element_text(margin = margin(r = 0)),
+        axis.text.y = element_text(
           size = 10,
           color = "black",
           margin = margin(b = 6)
         ),
         axis.title.x =  element_text(
-          size = 11,
+          size = 10,
           color = "black",
           hjust = 0.5
         ),
-        legend.position = "non"
-      ) +
-      coord_flip()
+        legend.position = "top",
+        legend.key.size = unit(0.1, "cm"),
+        legend.direction = "vertical",
+        legend.justification = "left",
+        legend.text = element_text(size = 8, face = "bold"),
+        legend.box.just = "right",
+        plot.margin = margin(0, 0.5, 0.5, 0, unit = "cm")
+      ) + coord_cartesian(clip = "off")
+
+
     ggsave("bar_errorbar.pdf",
            bar_errorbar,
            width = 8,
@@ -87,25 +178,28 @@ pathway_errorbar <-
 
 
 
-    daa_resuls_filtered_df <-
+    daa_results_filtered_sub_df <-
       cbind(
-        daa_resuls_filtered_df,
-        negative_log10_p = -log10(daa_resuls_filtered_df$p_adjust),
+        daa_results_filtered_sub_df,
+        negative_log10_p = -log10(daa_results_filtered_sub_df$p_adjust),
         group_nonsense = "nonsense"
       )
 
 
-    p_values_bar <- daa_resuls_filtered_df %>%
-      ggplot(aes(feature,negative_log10_p,fill=group_nonsense))+
-      geom_bar(stat="identity",position=position_dodge(width=0.8),width = 0.8)+
-      labs(y="P Value (-log 10)",x=NULL)+
+    p_values_bar <- daa_results_filtered_sub_df %>%
+      ggplot(aes(feature, negative_log10_p, fill = group_nonsense)) +
+      geom_bar(stat = "identity",
+               position = position_dodge(width = 0.8),
+               width = 0.8) +
+      labs(y = "P Value (-log 10)", x = NULL) +
       geom_stripped_cols() +
-      scale_fill_manual(values = c("#7fb1d3")) +
-      scale_color_manual(values = c("#7fb1d3")) +
-      geom_hline(aes(yintercept = 1.30103),linetype='dashed',color = 'black') +
+      scale_fill_manual(values = colors[3]) +
+      scale_color_manual(values = colors[3]) +
+      geom_hline(aes(yintercept = 1.30103),
+                 linetype = 'dashed',
+                 color = 'black') +
       theme_prism() +
       scale_y_continuous(expand = c(0, 0),
-                         #limits = c(0, 0.1),
                          guide = "prism_offset_minor") +
       labs(y = "P Values (-log10)", x = NULL) +
       theme(
@@ -130,39 +224,93 @@ pathway_errorbar <-
         legend.position = "non"
       ) +
       coord_flip()
-    ggsave("p_values_bar.pdf", p_values_bar, width = 8, height = 6)
+    ggsave("p_values_bar.pdf",
+           p_values_bar,
+           width = 8,
+           height = 6)
+
+    pathway_class_y <- (ymax + ymin) / 2 -0.5
+    pathway_class_plot_df <-
+      as.data.frame(
+        cbind(
+          nonsense = "nonsense",
+          pathway_class_y = pathway_class_y,
+          pathway_class = rev(unique(
+            daa_results_filtered_sub_df$pathway_class
+          ))
+        )
+      )
+    pathway_class_plot_df$pathway_class_y <-
+      as.numeric(pathway_class_plot_df$pathway_class_y)
+    pathway_class_annotation <-
+      pathway_class_plot_df %>% ggplot(aes(nonsense, pathway_class_y)) + geom_text(
+        aes(nonsense, pathway_class_y, label = pathway_class),
+        size = 3.5,
+        color = "black",
+        fontface = "bold",
+        family = "sans"
+      ) +
+      scale_y_discrete(position = "right") +
+      theme_prism() +
+      theme(
+        axis.ticks = element_blank(),
+        axis.line = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.background = element_blank(),
+        axis.text = element_blank(),
+        plot.margin = unit(c(0, 0.2, 0, 0), "cm"),
+        axis.title.y =  element_blank(),
+        axis.title.x = element_blank(),
+        legend.position = "non"
+      )
 
 
-    daa_resuls_filtered_df$p_adjust <- as.character(daa_resuls_filtered_df$p_adjust)
-    daa_resuls_filtered_df$unique <- nrow(daa_resuls_filtered_df)-order(daa_resuls_filtered_df$feature)+1
-    daa_resuls_filtered_df$p_adjust <- substr(daa_resuls_filtered_df$p_adjust,1,5)
-    p_annotation <- daa_resuls_filtered_df %>%
-      ggplot(aes(group_nonsense,p_adjust))+
-      geom_text(aes(group2,unique,label=p_adjust),size=3.5,color="black") +
-      labs(y="P_value")+
-      scale_y_discrete(position = "right")+
-      #scale_y_continuous(breaks = daa_resuls_filtered_df$p_adjust)+
-      theme_prism()+
-      theme(axis.ticks=element_blank(),
-            axis.line = element_blank(),
-            panel.grid.major.y =element_blank(),
-            panel.grid.major.x = element_blank(),
-            panel.background = element_blank(),
-            axis.text=element_blank(),
-            plot.margin = unit(c(0,0.2,0,0),"cm"),
-            axis.title.y =  element_text(size=11,color = "black",hjust = 0.5),
-            axis.title.x=element_blank(),
-            legend.position = "non")
 
 
 
 
-    bar_errorbar + p_values_bar + p_annotation
 
-
-
-
-    lefserPlot(Lefser_results,
-               colors = c("#7fb1d3", "#fdb462"),
-               trim.names = FALSE)
+    daa_results_filtered_sub_df$p_adjust <-
+      as.character(daa_results_filtered_sub_df$p_adjust)
+    daa_results_filtered_sub_df$unique <-
+      nrow(daa_results_filtered_sub_df) - seq_len(nrow(daa_results_filtered_sub_df)) + 1
+    daa_results_filtered_sub_df$p_adjust <-
+      substr(daa_results_filtered_sub_df$p_adjust, 1, 5)
+    p_annotation <- daa_results_filtered_sub_df %>%
+      ggplot(aes(group_nonsense, p_adjust)) +
+      geom_text(
+        aes(group_nonsense, unique, label = p_adjust),
+        size = 3.5,
+        color = "black",
+        fontface = "bold",
+        family = "sans"
+      ) +
+      labs(y = "p-value (corrected)") +
+      scale_y_discrete(position = "right") +
+      theme_prism() +
+      theme(
+        axis.ticks = element_blank(),
+        axis.line = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.background = element_blank(),
+        axis.text = element_blank(),
+        plot.margin = unit(c(0, 0.2, 0, 0), "cm"),
+        axis.title.y =  element_text(
+          size = 11,
+          color = "black",
+          hjust = 0.5
+        ),
+        axis.title.x = element_blank(),
+        legend.position = "non"
+      )
+    if (p_value_bar == TRUE) {
+      combination_bar_plot <-
+        bar_errorbar + p_values_bar + p_annotation + plot_layout(ncol = 3, width = c(2.3, 0.7, 0.3))
+    } else {
+      combination_bar_plot <-
+        bar_errorbar + p_annotation + plot_layout(ncol = 2, width = c(2.5,  0.2))
+    }
+    return(combination_bar_plot)
   }
